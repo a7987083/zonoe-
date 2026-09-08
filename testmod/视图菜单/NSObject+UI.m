6047b4756902cd739918a0e3ccef9de9f5a1c1da
a7987083 <85878547+a7987083@users.noreply.github.com>
2026-09-09 06:13:02 +0800

    diag: A request UDID and show success or failure popup

--- testmod/视图菜单/NSObject+UI.m
+++ testmod/视图菜单/NSObject+UI.m
@@ -7,113 +7,60 @@
 #import "JHDragView.h"
 #import "PopupMenuVC.h"
 #import "NSObject+UI.h"
-#import "../ZONServices/ZONUDIDBridge.h"
+#import "../ZONServices/ZONUDIDDiagUI.h"
 
 @implementation NSObject (UI)
-
-#pragma mark - 显示悬浮按钮
 
 - (void)显示图标
 {
     dispatch_async(dispatch_get_main_queue(), ^{
-
         UIViewController *vc = [self topViewController];
         UIView *parentView = vc.view;
-
         JHDragView *view = [parentView viewWithTag:100];
-
-        if (!view)
-        {
-            view = [[JHDragView alloc] initWithFrame:CGRectMake(
-                [UIScreen mainScreen].bounds.size.width - 70,
-                130,
-                50,
-                50
-            )];
-
+        if (!view) {
+            view = [[JHDragView alloc] initWithFrame:CGRectMake([UIScreen mainScreen].bounds.size.width - 70, 130, 50, 50)];
             view.tag = 100;
-
             [parentView addSubview:view];
         }
     });
 }
 
-#pragma mark - 打开菜单
-
 - (void)vip菜单显示
 {
     dispatch_async(dispatch_get_main_queue(), ^{
-
         UIViewController *topVC = [self topViewController];
-        if (!topVC || [topVC isKindOfClass:[PopupMenuVC class]]) {
-            return;
-        }
+        if (!topVC || [topVC isKindOfClass:[PopupMenuVC class]]) return;
 
         PopupMenuVC *menu = [PopupMenuVC new];
         menu.modalPresentationStyle = UIModalPresentationOverFullScreen;
+        if (@available(iOS 13.0, *)) menu.overrideUserInterfaceStyle = UIUserInterfaceStyleLight;
 
-        if (@available(iOS 13.0, *)) {
-            menu.overrideUserInterfaceStyle = UIUserInterfaceStyleLight;
-        }
-
-        [topVC presentViewController:menu
-                            animated:NO
-                          completion:^{
-            // Diagnostic A: install callback hook only. Do not open zonoe.
-            if (ZONUDIDBridgeCallbackScheme().length > 0) {
-                dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(350 * NSEC_PER_MSEC)),
-                               dispatch_get_main_queue(), ^{
-                    NSLog(@"[zonoemenu][DIAG][udid] hook-only begin");
-                    ZONUDIDBridgeInstallDelegateHooks();
-                    NSLog(@"[zonoemenu][DIAG][udid] hook-only complete");
-                });
-            }
+        [topVC presentViewController:menu animated:NO completion:^{
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(350 * NSEC_PER_MSEC)),
+                           dispatch_get_main_queue(), ^{
+                ZONUDIDDiagRun(@"A / Delegate Hook");
+            });
         }];
     });
 }
 
-- (void)vipaa
-{
-    [self vip菜单显示];
-}
-
-#pragma mark - 获取顶部控制器
+- (void)vipaa { [self vip菜单显示]; }
 
 - (UIViewController *)topViewController
 {
     UIViewController *rootVC = nil;
-
-    if (@available(iOS 13.0, *))
-    {
-        for (UIScene *scene in UIApplication.sharedApplication.connectedScenes)
-        {
-            if ([scene isKindOfClass:UIWindowScene.class] &&
-                scene.activationState == UISceneActivationStateForegroundActive)
-            {
+    if (@available(iOS 13.0, *)) {
+        for (UIScene *scene in UIApplication.sharedApplication.connectedScenes) {
+            if ([scene isKindOfClass:UIWindowScene.class] && scene.activationState == UISceneActivationStateForegroundActive) {
                 UIWindowScene *windowScene = (UIWindowScene *)scene;
-
-                for (UIWindow *window in windowScene.windows)
-                {
-                    if (window.isKeyWindow)
-                    {
-                        rootVC = window.rootViewController;
-                        break;
-                    }
+                for (UIWindow *window in windowScene.windows) {
+                    if (window.isKeyWindow) { rootVC = window.rootViewController; break; }
                 }
             }
         }
     }
-
-    if (!rootVC)
-    {
-        rootVC = UIApplication.sharedApplication.keyWindow.rootViewController;
-    }
-
-    while (rootVC.presentedViewController)
-    {
-        rootVC = rootVC.presentedViewController;
-    }
-
+    if (!rootVC) rootVC = UIApplication.sharedApplication.keyWindow.rootViewController;
+    while (rootVC.presentedViewController) rootVC = rootVC.presentedViewController;
     return rootVC;
 }
 
