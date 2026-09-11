abab876c24cdf78e10d44aaca1fe2f7ad1527e1e
a7987083 <85878547+a7987083@users.noreply.github.com>
2026-09-11 21:05:48 +0800

    refactor(menu): make PopupMenuVC a coordinator shell

--- testmod/菜单/PopupMenuVC.m
+++ testmod/菜单/PopupMenuVC.m
@@ -1,104 +1,60 @@
 #import "PopupMenuVC.h"
-#import "FoldSectionView.h"
-#import "../ZONCore/ZONSectionRenderer.h"
-#import "../ZONCore/ZONMenuChromeRenderer.h"
-#import "../ZONCore/ZONMenuPanelController.h"
-#import "../ZONCore/ZONMenuEventBridge.h"
+#import "../ZONCore/ZONMenuCoordinator.h"
 
-@interface PopupMenuVC () <UIGestureRecognizerDelegate>
-@property(nonatomic,strong) UIView *panel;
-@property(nonatomic,strong) UIScrollView *scroll;
-@property(nonatomic,strong) NSMutableArray<FoldSectionView *> *sections;
-@property(nonatomic, assign) BOOL didBuildUI;
+@interface PopupMenuVC ()
+@property(nonatomic,strong) ZONMenuCoordinator *menuCoordinator;
 @end
 
 @implementation PopupMenuVC
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.view.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:0.35];
-
-    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(close)];
-    tap.delegate = self;
-    tap.cancelsTouchesInView = NO;
-    [self.view addGestureRecognizer:tap];
-
-    self.panel = ZONCreateMenuPanel(self.view, &_scroll);
-    [self buildUI];
-}
-
-- (void)relayoutSections {
-    ZONRelayoutMenuSections(self.panel, self.scroll, self.sections);
-}
-
-- (void)buildUI {
-    if (self.didBuildUI) return;
-    self.didBuildUI = YES;
-
-    if (!self.sections) self.sections = [NSMutableArray array];
-    [self.sections removeAllObjects];
-    for (UIView *v in self.scroll.subviews) [v removeFromSuperview];
-    self.scroll.showsVerticalScrollIndicator = YES;
-
-    ZONRenderMenuHeader(self.panel, self.scroll);
-
-    __weak typeof(self) weakSelf = self;
-    NSArray<FoldSectionView *> *rendered = ZONRenderRegisteredSections(self.scroll,
-                                                                       self.panel.bounds.size.width,
-                                                                       self,
-                                                                       @selector(cardButtonTap:),
-                                                                       @selector(gridButtonTap:),
-                                                                       @selector(switchChanged:),
-                                                                       @selector(adSwitchChanged:),
-                                                                       @selector(adSliderChanged:),
-                                                                       ^{
-        [weakSelf relayoutSections];
-    });
-    [self.sections addObjectsFromArray:rendered];
-}
-
-- (void)adSwitchChanged:(UISwitch *)sw {
-    ZONMenuHandleAdSwitch(sw);
-}
-
-- (void)adSliderChanged:(UISlider *)slider {
-    ZONMenuHandleAdSlider(slider);
-}
-
-- (void)cardButtonTap:(UIButton *)sender {
-    ZONMenuHandleAction(sender.tag, self);
-}
-
-- (void)gridButtonTap:(UIButton *)sender {
-    ZONMenuHandleAction(sender.tag, self);
-}
-
-- (void)switchChanged:(UISwitch *)sw {
-    ZONMenuHandleToggle(sw.tag, sw.isOn);
+    self.menuCoordinator = [[ZONMenuCoordinator alloc] initWithPresenter:self];
+    [self.menuCoordinator viewDidLoad];
 }
 
 - (void)viewDidAppear:(BOOL)animated {
     [super viewDidAppear:animated];
-    ZONMenuSyncSettingsToRuntime();
-    ZONShowMenuPanel(self.view, self.panel);
+    [self.menuCoordinator viewDidAppear];
 }
 
 - (void)viewWillLayoutSubviews {
     [super viewWillLayoutSubviews];
-    ZONLayoutVisibleMenuPanel(self.view, self.panel, self.scroll);
-    [self relayoutSections];
+    [self.menuCoordinator viewWillLayoutSubviews];
+}
+
+#pragma mark - Legacy selector passthrough
+
+- (void)buildUI {
+    [self.menuCoordinator buildUI];
+}
+
+- (void)relayoutSections {
+    [self.menuCoordinator relayoutSections];
 }
 
 - (void)close {
-    __weak typeof(self) weakSelf = self;
-    ZONHideMenuPanel(self.view, self.panel, ^{
-        [weakSelf dismissViewControllerAnimated:NO completion:nil];
-    });
+    [self.menuCoordinator close];
 }
 
-- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldReceiveTouch:(UITouch *)touch {
-    (void)gestureRecognizer;
-    return !ZONMenuPanelContainsTouch(self.view, self.panel, touch);
+- (void)adSwitchChanged:(UISwitch *)sw {
+    [self.menuCoordinator adSwitchChanged:sw];
+}
+
+- (void)adSliderChanged:(UISlider *)slider {
+    [self.menuCoordinator adSliderChanged:slider];
+}
+
+- (void)cardButtonTap:(UIButton *)sender {
+    [self.menuCoordinator cardButtonTap:sender];
+}
+
+- (void)gridButtonTap:(UIButton *)sender {
+    [self.menuCoordinator gridButtonTap:sender];
+}
+
+- (void)switchChanged:(UISwitch *)sw {
+    [self.menuCoordinator switchChanged:sw];
 }
 
 @end
